2e6313df42c86036addefeea54890267d5ddd74d
gxswift <853129649@qq.com>
2020-12-10 23:57:23 +0800

    add isp software

---
+++ 参考/source/lcd.c
@@ -0,0 +1,455 @@
+#include "lcd.h"
+#include "stdlib.h"
+#include "usart.h"	 
+#include "delay.h"
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//测试硬件：单片机STM32F103RZET6,正点原子MiniSTM32开发板,主频72MHZ
+//中景园液晶驱动 for STM32 IO模拟
+//淘宝网站：https://shop73023976.taobao.com/?spm=2013.1.1000126.d21.dSpP3S
+//我司提供技术支持，任何技术问题欢迎随时交流学习
+//技术交流QQ群:2534656669
+//创建日期:2015/5/5
+//版本：V1.1
+//版权所有，盗版必究。
+//Copyright(C) 中景园电子
+//All rights reserved
+//All rights reserved
+/****************************************************************************************************
+//=======================================液晶屏数据线接线==========================================//
+//DB0       接PD14 
+//DB1       接PD15 
+//DB2       接PD0 
+//DB3       接PD1 
+//DB4~DB12  依次接PE7~PE15
+//DB13      接PD8 
+//DB14      接PD9
+//DB15      接PD10  
+//=======================================液晶屏控制线接线==========================================//
+//LCD_CS	接PG12	//片选信号
+//LCD_RS	接PG0	//寄存器/数据选择信号
+//LCD_WR	接PD5	//写信号
+//LCD_RD	接PD4	//读信号
+//LCD_RST	接PC5	//复位信号
+//LCD_LED	接PB0	//背光控制信号(高电平点亮)
+//=========================================触摸屏触接线=========================================//
+//不使用触摸或者模块本身不带触摸，则可不连接
+//MO(MISO)	接PF8	//SPI总线输出
+//MI(MOSI)	接PF9	//SPI总线输入
+//PEN		接PF10	//触摸屏中断信号
+//TCS		接PB2	//触摸IC片选
+//CLK		接PB1	//SPI总线时钟
+**************************************************************************************************/	
+	   
+//管理LCD重要参数
+//默认为竖屏
+_lcd_dev lcddev;
+
+//画笔颜色,背景颜色
+u16 POINT_COLOR = 0x0000,BACK_COLOR = 0xFFFF;  
+u16 DeviceCode;	 
+
+
+//******************************************************************
+//函数名：  LCD_WR_REG
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    向液晶屏总线写入写16位指令
+//输入参数：Reg:待写入的指令值
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_WR_REG(u16 data)
+{ 
+	LCD->LCD_REG=data;//写入要写的寄存器序号	 	
+}
+
+//******************************************************************
+//函数名：  LCD_WR_DATA
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    向液晶屏总线写入写16位数据
+//输入参数：Data:待写入的数据
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_WR_DATA(u16 data)
+{
+	LCD->LCD_RAM=data;
+
+}
+//******************************************************************
+//函数名：  LCD_DrawPoint_16Bit
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    写入一个16位数据
+//输入参数：(x,y):光标坐标
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_DrawPoint_16Bit(u16 color)
+{
+	LCD_WR_DATA(color); 
+}
+
+//******************************************************************
+//函数名：  LCD_WriteReg
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    写寄存器数据
+//输入参数：LCD_Reg:寄存器地址
+//			LCD_RegValue:要写入的数据
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_WriteReg(u16 LCD_Reg, u16 LCD_RegValue)
+{	
+	LCD->LCD_REG = LCD_Reg;		//写入要写的寄存器序号	 
+	LCD->LCD_RAM = LCD_RegValue;//写入数据	     		 
+}		   
+	 
+//******************************************************************
+//函数名：  LCD_WriteRAM_Prepare
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    开始写GRAM
+//			在给液晶屏传送RGB数据前，应该发送写GRAM指令
+//输入参数：无
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_WriteRAM_Prepare(void)
+{
+	LCD_WR_REG(lcddev.wramcmd);
+}	 
+
+//******************************************************************
+//函数名：  LCD_DrawPoint
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    在指定位置写入一个像素点数据
+//输入参数：(x,y):光标坐标
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_DrawPoint(u16 x,u16 y)
+{
+	LCD_SetCursor(x,y);
+	LCD_WR_DATA(POINT_COLOR); 
+}
+
+//******************************************************************
+//函数名：  LCD_Clear
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    LCD全屏填充清屏函数
+//输入参数：Color:要清屏的填充色
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_Clear(u16 Color)
+{	
+	u32 index=0; 
+	     
+	LCD_SetWindows(0,0,lcddev.width-1,lcddev.height-1);	
+
+	for(index=0;index<153600;index++)
+	{
+		LCD->LCD_RAM=Color;		  
+	}
+} 
+//初始化lcd
+//该初始化函数可以初始化各种ILI93XX液晶,但是其他函数是基于ILI9320的!!!
+//在其他型号的驱动芯片上没有测试! 
+void LCD_Init(void)
+{ 	
+	vu32 i=0;
+	
+  GPIO_InitTypeDef  GPIO_InitStructure;
+	FSMC_NORSRAMInitTypeDef  FSMC_NORSRAMInitStructure;
+  FSMC_NORSRAMTimingInitTypeDef  readWriteTiming; 
+	FSMC_NORSRAMTimingInitTypeDef  writeTiming;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB|RCC_AHB1Periph_GPIOD|RCC_AHB1Periph_GPIOE|RCC_AHB1Periph_GPIOF|RCC_AHB1Periph_GPIOG, ENABLE);//使能PD,PE,PF,PG时钟  
+  RCC_AHB3PeriphClockCmd(RCC_AHB3Periph_FSMC,ENABLE);//使能FSMC时钟  
+	
+ 
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;//PB15 推挽输出,控制背光
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;//100MHz
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+  GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化 //PB15 推挽输出,控制背光
+	
+  GPIO_InitStructure.GPIO_Pin = (3<<0)|(3<<4)|(7<<8)|(3<<14);//PD0,1,4,5,8,9,10,14,15 AF OUT
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用输出
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+  GPIO_Init(GPIOD, &GPIO_InitStructure);//初始化  
+	
+  GPIO_InitStructure.GPIO_Pin = (0X1FF<<7);//PE7~15,AF OUT
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用输出
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+  GPIO_Init(GPIOE, &GPIO_InitStructure);//初始化  
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;//PF12,FSMC_A6
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用输出
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+  GPIO_Init(GPIOF, &GPIO_InitStructure);//初始化  
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;//PF12,FSMC_A6
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用输出
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+  GPIO_Init(GPIOG, &GPIO_InitStructure);//初始化 
+
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource0,GPIO_AF_FSMC);//PD0,AF12
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource1,GPIO_AF_FSMC);//PD1,AF12
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource4,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource5,GPIO_AF_FSMC); 
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource8,GPIO_AF_FSMC); 
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource9,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource10,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource14,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOD,GPIO_PinSource15,GPIO_AF_FSMC);//PD15,AF12
+ 
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource7,GPIO_AF_FSMC);//PE7,AF12
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource8,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource9,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource10,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource11,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource12,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource13,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource14,GPIO_AF_FSMC);
+  GPIO_PinAFConfig(GPIOE,GPIO_PinSource15,GPIO_AF_FSMC);//PE15,AF12
+ 
+  GPIO_PinAFConfig(GPIOF,GPIO_PinSource12,GPIO_AF_FSMC);//PF12,AF12
+  GPIO_PinAFConfig(GPIOG,GPIO_PinSource12,GPIO_AF_FSMC);
+
+
+  readWriteTiming.FSMC_AddressSetupTime = 0XF;	 //地址建立时间（ADDSET）为16个HCLK 1/168M=6ns*16=96ns	
+  readWriteTiming.FSMC_AddressHoldTime = 0x00;	 //地址保持时间（ADDHLD）模式A未用到	
+  readWriteTiming.FSMC_DataSetupTime = 60;			//数据保存时间为60个HCLK	=6*60=360ns
+  readWriteTiming.FSMC_BusTurnAroundDuration = 0x00;
+  readWriteTiming.FSMC_CLKDivision = 0x00;
+  readWriteTiming.FSMC_DataLatency = 0x00;
+  readWriteTiming.FSMC_AccessMode = FSMC_AccessMode_A;	 //模式A 
+    
+
+	writeTiming.FSMC_AddressSetupTime =9;	      //地址建立时间（ADDSET）为9个HCLK =54ns 
+  writeTiming.FSMC_AddressHoldTime = 0x00;	 //地址保持时间（A		
+  writeTiming.FSMC_DataSetupTime = 8;		 //数据保存时间为6ns*9个HCLK=54ns
+  writeTiming.FSMC_BusTurnAroundDuration = 0x00;
+  writeTiming.FSMC_CLKDivision = 0x00;
+  writeTiming.FSMC_DataLatency = 0x00;
+  writeTiming.FSMC_AccessMode = FSMC_AccessMode_A;	 //模式A 
+
+ 
+  FSMC_NORSRAMInitStructure.FSMC_Bank = FSMC_Bank1_NORSRAM4;//  这里我们使用NE4 ，也就对应BTCR[6],[7]。
+  FSMC_NORSRAMInitStructure.FSMC_DataAddressMux = FSMC_DataAddressMux_Disable; // 不复用数据地址
+  FSMC_NORSRAMInitStructure.FSMC_MemoryType =FSMC_MemoryType_SRAM;// FSMC_MemoryType_SRAM;  //SRAM   
+  FSMC_NORSRAMInitStructure.FSMC_MemoryDataWidth = FSMC_MemoryDataWidth_16b;//存储器数据宽度为16bit   
+  FSMC_NORSRAMInitStructure.FSMC_BurstAccessMode =FSMC_BurstAccessMode_Disable;// FSMC_BurstAccessMode_Disable; 
+  FSMC_NORSRAMInitStructure.FSMC_WaitSignalPolarity = FSMC_WaitSignalPolarity_Low;
+	FSMC_NORSRAMInitStructure.FSMC_AsynchronousWait=FSMC_AsynchronousWait_Disable; 
+  FSMC_NORSRAMInitStructure.FSMC_WrapMode = FSMC_WrapMode_Disable;   
+  FSMC_NORSRAMInitStructure.FSMC_WaitSignalActive = FSMC_WaitSignalActive_BeforeWaitState;  
+  FSMC_NORSRAMInitStructure.FSMC_WriteOperation = FSMC_WriteOperation_Enable;	//  存储器写使能
+  FSMC_NORSRAMInitStructure.FSMC_WaitSignal = FSMC_WaitSignal_Disable;   
+  FSMC_NORSRAMInitStructure.FSMC_ExtendedMode = FSMC_ExtendedMode_Enable; // 读写使用不同的时序
+  FSMC_NORSRAMInitStructure.FSMC_WriteBurst = FSMC_WriteBurst_Disable; 
+  FSMC_NORSRAMInitStructure.FSMC_ReadWriteTimingStruct = &readWriteTiming; //读写时序
+  FSMC_NORSRAMInitStructure.FSMC_WriteTimingStruct = &writeTiming;  //写时序
+
+  FSMC_NORSRAMInit(&FSMC_NORSRAMInitStructure);  //初始化FSMC配置
+
+  FSMC_NORSRAMCmd(FSMC_Bank1_NORSRAM4, ENABLE);  // 使能BANK1 
+	
+	LCD_WR_REG(0XF1);
+	LCD_WR_DATA(0x36);
+	LCD_WR_DATA(0x04);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x3C);
+	LCD_WR_DATA(0x0F);
+	LCD_WR_DATA(0x8F);
+
+	LCD_WR_REG(0XF2);
+	LCD_WR_DATA(0x18);
+	LCD_WR_DATA(0xA3);
+	LCD_WR_DATA(0x12);
+	LCD_WR_DATA(0x02);
+	LCD_WR_DATA(0xB2);
+	LCD_WR_DATA(0x12);
+	LCD_WR_DATA(0xFF);
+	LCD_WR_DATA(0x10);
+	LCD_WR_DATA(0x00);
+
+	LCD_WR_REG(0XF8);
+	LCD_WR_DATA(0x21);
+	LCD_WR_DATA(0x04);
+
+	LCD_WR_REG(0XF9);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x08);
+
+	LCD_WR_REG(0xE0);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x10);
+	LCD_WR_DATA(0x0F);
+	LCD_WR_DATA(0x0E);
+	LCD_WR_DATA(0x11);
+	LCD_WR_DATA(0x0A);
+	LCD_WR_DATA(0x44);
+	LCD_WR_DATA(0x77);
+	LCD_WR_DATA(0x38);
+	LCD_WR_DATA(0x08);
+	LCD_WR_DATA(0x13);
+	LCD_WR_DATA(0x05);
+	LCD_WR_DATA(0x0B);
+	LCD_WR_DATA(0x02);
+	LCD_WR_DATA(0x00);
+
+	LCD_WR_REG(0XE1);
+	LCD_WR_DATA(0x0F);
+	LCD_WR_DATA(0x3B);
+	LCD_WR_DATA(0x39);
+	LCD_WR_DATA(0x0D);
+	LCD_WR_DATA(0x0F);
+	LCD_WR_DATA(0x05);
+	LCD_WR_DATA(0x53);
+	LCD_WR_DATA(0x43);
+	LCD_WR_DATA(0x3D);
+	LCD_WR_DATA(0x05);
+	LCD_WR_DATA(0x0F);
+	LCD_WR_DATA(0x04);
+	LCD_WR_DATA(0x23);
+	LCD_WR_DATA(0x1F);
+	LCD_WR_DATA(0x0F);
+
+	LCD_WR_REG(0x36);
+	LCD_WR_DATA(0x08);
+
+	LCD_WR_REG(0x3A);
+	LCD_WR_DATA(0x55); // 8bit,16bit=0x55 18bit=0x66
+
+	LCD_WR_REG(0XB1);    // Frame rate 70Hz b0 11
+	LCD_WR_DATA(0xB0);
+	LCD_WR_DATA(0x11); 
+
+	LCD_WR_REG(0XB4);
+	LCD_WR_DATA(0x02);
+
+	LCD_WR_REG(0XB6);
+	LCD_WR_DATA(0x00);  //MCU:0x02  RGB:0x22
+	LCD_WR_DATA(0x42);  // rotate 180 degree 0x42
+	LCD_WR_DATA(0x3B);
+
+	LCD_WR_REG(0XB7);
+	LCD_WR_DATA(0xC6);
+
+	LCD_WR_REG(0XC0);
+	LCD_WR_DATA(0x05);
+	LCD_WR_DATA(0x05);
+
+	LCD_WR_REG(0XC1);
+	LCD_WR_DATA(0x41);
+	LCD_WR_DATA(0x00);
+
+	LCD_WR_REG(0XC2);
+	LCD_WR_DATA(0x22);
+
+	LCD_WR_REG(0XC5);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x31);  // Adjust Flicker
+	LCD_WR_DATA(0x80); 
+		
+	LCD_WR_REG(0x11);
+	delay_ms(120);
+	LCD_WR_REG(0x29);
+
+	LCD_SetParam();//设置LCD参数	 
+	LCD_LED=1;//点亮背光	 
+	LCD_Clear(WHITE);
+}  
+ 
+//******************************************************************
+//函数名：  LCD_SetWindows
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    设置显示窗口
+//输入参数：(xStar,yStar):窗口左上角起始坐标
+//	 	   	(xEnd,yEnd):窗口右下角结束坐标
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_SetWindows(u16 xStar, u16 yStar,u16 xEnd,u16 yEnd)
+{	
+	LCD_WR_REG(lcddev.setxcmd);	
+	LCD_WR_DATA(xStar>>8);
+	LCD_WR_DATA(0x00FF&xStar);		
+	LCD_WR_DATA(xEnd>>8);
+	LCD_WR_DATA(0x00FF&xEnd);
+
+	LCD_WR_REG(lcddev.setycmd);	
+	LCD_WR_DATA(yStar>>8);
+	LCD_WR_DATA(0x00FF&yStar);		
+	LCD_WR_DATA(yEnd>>8);
+	LCD_WR_DATA(0x00FF&yEnd);
+
+	LCD_WriteRAM_Prepare();	//开始写入GRAM			
+}   
+
+//******************************************************************
+//函数名：  LCD_SetCursor
+//作者：   中景园电子
+//日期：    2013-02-22
+//功能：    设置光标位置
+//输入参数：Xpos:横坐标
+//			Ypos:纵坐标
+//返回值：  无
+//修改记录：无
+//******************************************************************
+void LCD_SetCursor(u16 Xpos, u16 Ypos)
+{	  	    			
+	LCD_WR_REG(lcddev.setxcmd);	
+	LCD_WR_DATA(Xpos>>8);
+	LCD_WR_DATA(0x00FF&Xpos);		
+	LCD_WR_DATA((Xpos+1)>>8);
+	LCD_WR_DATA((Xpos+1));
+	
+	LCD_WR_REG(lcddev.setycmd);	
+	LCD_WR_DATA(Ypos>>8);
+	LCD_WR_DATA(0x00FF&Ypos);		
+	LCD_WR_DATA((Ypos+1)>>8);
+	LCD_WR_DATA((Ypos+1));
+	LCD_WriteRAM_Prepare();	//开始写入GRAM		
+} 
+
+
+void LCD_SetParam(void)
+{ 
+	lcddev.setxcmd=0x2A;
+	lcddev.setycmd=0x2B;
+	lcddev.wramcmd=0x2C;
+#if USE_HORIZONTAL==1	//使用横屏	  
+	lcddev.dir=1;//横屏
+	lcddev.width=480;
+	lcddev.height=320;			
+	LCD_WriteReg(0x36,(1<<3)|(1<<7)|(1<<5));//BGR==1,MY==1,MX==0,MV==1
+#else//竖屏
+	lcddev.dir=0;//竖屏				 	 		
+	lcddev.width=320;
+	lcddev.height=480;	
+	LCD_WriteReg(0x36,(1<<3)|(1<<6)|(1<<7));//BGR==1,MY==0,MX==0,MV==0
+#endif
+}	
+
+
+
+
+
