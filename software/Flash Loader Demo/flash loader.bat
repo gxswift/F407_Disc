b92ade789eb1ef5c3451f5dc14b5fb8c5fe7bbc6
gxswift <853129649@qq.com>
2021-05-30 13:32:06 +0800

    add bat file

---
+++ software/Flash Loader Demo/flash loader.bat
@@ -0,0 +1,6 @@
+cls
+@echo off
+echo DTR Low reset RTS High Bootloader
+echo -----------------------------------------------
+STMFlashLoader.exe -c --pn 5 --br 115200 --db 8 --pr EVEN --sb 1 --ec OFF  --to 10000 --co ON -Dtr --Lo -Rts --Hi -Dtr --Hi -i STM32F4_05_07_15_17_1024K -e --all -d --fn ..\..\build\F407.hex  -Dtr --Lo -Rts --Lo -Dtr --Hi
+PAUSE 
